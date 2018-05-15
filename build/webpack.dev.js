8a1f7560af3f40b3fa967ec7890777a62f81c8c2
yangbing36 <yangbing5@jd.com>
2018-05-15 21:43:17 +0800

    build

---
+++ build/webpack.dev.js
@@ -0,0 +1,39 @@
+const webpack = require('webpack');
+const path = require('path');
+const htmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    mode: 'development',
+    devServer: {
+        contentBase: path.resolve(__dirname, '../')
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: path.resolve(__dirname, '../node_modules'),
+                loader: 'babel-loader'
+            },
+            {
+                test: /\.vue$/,
+                loader: 'vue-loader'
+            },
+            {
+                test: /\.css$/,
+                loader: 'style-loader!css-loader'
+            }
+        ]
+    },
+    resolve: {
+        alias:{
+            'vue$': 'vue/dist/vue.esm.js'
+        }
+    },
+    plugins: [
+        new htmlWebpackPlugin({
+            template: path.resolve(__dirname, '../src/index.html'),
+            inject: true
+        })
+    ]
+
+}
---
+++ src/app.vue
@@ -0,0 +1,11 @@
+<template>
+    <div>
+        <canvas id="myCanvas"></canvas>
+    </div>
+</template>
+
+<script>
+    export default {
+
+    }
+</script>
